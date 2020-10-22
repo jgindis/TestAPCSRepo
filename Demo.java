95329c13a4678f9d25798c788241320a0143d2dd
jgindis <jgindis20@stuy.edu>
2020-10-22 13:03:47 -0400

    Updated this program for Java HW#10

--- Demo.java
+++ Demo.java
@@ -1,13 +1,65 @@
 public class Demo {
-    public static void main(String[] args) {
-      Demo(5);
-    }
-      public static void Demo(int n) {
-      for(int i = 1; i < n; i++){
-        for(int j = 0; j < i; j++){
-          System.out.print(i);
-        }
-        System.out.println("");
+  public static void main(String[] args) {
+  }
+
+  public static void printLoop(int n) {
+    for (int i = 1; i < n+1; i++) {
+      for (int j = 1; j <= i; j++) {
+         System.out.print(n-i+1);
       }
+      System.out.println();
     }
+  }
+
+  public static String arrToString(int[]arr){
+      String str = "{";
+      if (arr.length == 0) {
+         str += "}";
+         return str;
+      }
+      for (int i = 0; i < arr.length-1; i++) {
+        str = str + String.valueOf(arr[i]) + ", ";
+      }
+      str = str + String.valueOf(arr[arr.length-1]) + "}";
+      return str;
+  }
+
+  public static String arrayDeepToString(int[][]arr) {
+    String str = "{";
+    if (arr.length == 0) return (str + "}");
+    for (int i = 0; i < arr.length-1; i++) {
+      str += arrToString(arr[i]) + ", ";
+    }
+    str += arrToString(arr[arr.length-1]) + "}";
+    return str;
+  }
+
+  public static int[][] create2DArray(int row, int col, int len) {
+    int[][] arr = new int[row][col];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            double rand = Math.random() * len++;
+            double round = Math.floor(rand);
+            int str = (int)round;
+            arr[i][j] = str;
+          }
+        }
+        return arr;
+      }
+
+  public static int[][] create2DArrayrand(int row, int col, int len) {
+    int[][] arr = new int[row][];
+    for (int i = 0; i < row; i++) {
+      double randcol = Math.random() * (col + 1);
+      int cols = (int) randcol;
+      arr[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+          double randomNum = Math.random() * len++;
+          int str = (int)randomNum;
+          arr[i][j] = str;
+        }
+      }
+      return arr;
+    }
+
 }
